09de43a392afbc85b0b5e0cda37a9faad8e2d1c9
hector-leubeck <hector.leubeck.lux.renatus@gmail.com>
2023-01-26 16:18:06 -0300

    feat: create 'comments' app

---
+++ comments/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Comments
+
+
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('com_name', 'com_post', 'com_date', 'com_pub')
+    list_display_links = ['com_name', 'com_post']
+    list_editable = ['com_pub']
+
+
+admin.site.register(Comments, CommentsAdmin)
---
+++ comments/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from posts.models import Post
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+class Comments(models.Model):
+    com_name = models.CharField(max_length=200, verbose_name='Nome')
+    com_email = models.EmailField
+    commentary = models.TextField(verbose_name='Comentário')
+    com_post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                                 verbose_name='Post')
+    com_user = models.ForeignKey(User, on_delete=models.DO_NOTHING,
+                                 verbose_name='Autor')
+    com_date = models.DateTimeField(default=timezone.now, verbose_name='Data')
+    com_pub = models.BooleanField(default=False, verbose_name='Publicado')
+
+    def __str__(self) -> str:
+        return self.com_name
