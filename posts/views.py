1c40a5b5e66082dc6ac8f349e0242501de415a8f
hector-leubeck <hector.leubeck.lux.renatus@gmail.com>
2023-02-03 18:02:47 -0300

    feat: turn buttons from home functional

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
-from django.db.models import Case, When, Count
+from django.db.models import Case, When, Count, Q
 
 
 class PostIndex(ListView):
@@ -19,11 +19,31 @@ class PostIndex(ListView):
 
 
 class PostSearch(PostIndex):
-    template_name = 'post/post_search.html'
+    template_name = 'posts/post_search.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        searching = (self.request.GET.get('termo'))
+        if not searching:
+            return qs
+
+        qs = qs.filter(Q(post_title__icontains=searching) |
+                       Q(post_content__icontains=searching) |
+                       Q(post_author__first_name__iexact=searching) |
+                       Q(post_category__cat_name__iexact=searching))
+        return qs
 
 
 class PostCategories(PostIndex):
     template_name = 'posts/post_category.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        category = self.kwargs.get('category')
+        if not category:
+            return qs
+        qs = qs.filter(post_category__cat_name__iexact=category)
+        return qs
 
 
 class PostDetails(UpdateView):
