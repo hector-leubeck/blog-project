ab07a212a7fae129adb6e9ef35959f7774a2967e
hector-leubeck <hector.leubeck.lux.renatus@gmail.com>
2023-02-03 15:52:39 -0300

    feat: implement Post categories and post search

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from .models import Post
+from django.db.models import Case, When, Count
 
 
 class PostIndex(ListView):
@@ -9,13 +10,20 @@ class PostIndex(ListView):
     paginate_by = 6
     context_object_name = 'posts'
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs.order_by('-id').filter(post_pub=True)
+        qs.annotate(
+            comments_pub=Count(Case(When(comments__com_pub=True, then=1))))
+        return qs
+
 
 class PostSearch(PostIndex):
-    pass
+    template_name = 'post/post_search.html'
 
 
 class PostCategories(PostIndex):
-    pass
+    template_name = 'posts/post_category.html'
 
 
 class PostDetails(UpdateView):
