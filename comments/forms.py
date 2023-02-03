1ce50cbd91e331972595ecc74a9913af6c7c1e31
hector-leubeck <hector.leubeck.lux.renatus@gmail.com>
2023-02-03 18:41:43 -0300

    feat: implement commentary validantions

--- comments/forms.py
+++ comments/forms.py
@@ -3,6 +3,16 @@ from .models import Comments
 
 
 class FormComment(ModelForm):
+    def clean(self):
+        data = self.cleaned_data
+        name = data.get('com_name')
+        comment = data.get('commentary')
+
+        if len(name) < 2:
+            self.add_error('com_name', 'Nome inválido!')
+        if len(comment) < 10:
+            self.add_error('commentary', 'Comentário Inválido!')
+
     class Meta:
         model = Comments
         fields = ('com_name', 'com_email', 'commentary')
